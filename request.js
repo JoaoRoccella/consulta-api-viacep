fa651de1b2f355917afdaa8e3da0801c802adc3e
Joao Roccella <joao.marcos@sp.senai.br>
2023-06-28 17:17:02 -0300

    Melhorando a consulta à API

---
+++ request.js
@@ -0,0 +1,19 @@
+const request = new Request("https://viacep.com.br/ws/08330900/json/", {
+  method: "GET",
+});
+
+fetch(request)
+  .then((response) => {
+    if (response.status === 200) {
+      return response.json();
+    } else {
+      throw new Error("Something went wrong on API server!");
+    }
+  })
+  .then((response) => {
+    console.log(response);
+    // …
+  })
+  .catch((error) => {
+    console.error(error);
+  });
